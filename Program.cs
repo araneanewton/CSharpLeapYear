caa81d32b9b5158416109617894f8da5f02e6ac9
araneanewton <araneanewton@gmail.com>
2025-05-14 11:22:44 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Nhap nam muon kiem tra: ");
+        Console.Write("Nhap nam muon kiem tra: ");
         int year = Convert.ToInt32(Console.ReadLine());
         bool laNamNhuan = false;
 
